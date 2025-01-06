2d4f42d563d8a118acf47b37786b34fdc283ab0a
Reed Whetstone <rwhetstone0934@gmail.com>
2025-01-05 23:24:35 -0700

    cleaned up table code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -116,105 +116,37 @@
 		</button>
 
 		<!-- Table with a reactive class binding -->
-		<table class:hidden={isHidden} class="table-auto rounded">
-			<thead class="bg-gray-700 text-xs uppercase text-gray-400">
-				<tr>
-					<th class=" px-6 py-3">ID</th>
-					<th class=" px-6 py-3">Name</th>
-					<th class=" px-6 py-3">Rank</th>
-					<th class=" px-6 py-3">Notes</th>
-					<th class=" px-6 py-3">Purchase Date</th>
-					<th class=" px-6 py-3">Arrival Date</th>
-					<th class=" px-6 py-3">Region</th>
-					<th class=" px-6 py-3">Processing</th>
-					<th class=" px-6 py-3">Drying Method</th>
-					<th class=" px-6 py-3">Lot Size</th>
-					<th class=" px-6 py-3">Bag Size</th>
-					<th class=" px-6 py-3">Packaging</th>
-					<th class=" px-6 py-3">Farm Gate</th>
-					<th class=" px-6 py-3">Cultivar Detail</th>
-					<th class=" px-6 py-3">Grade</th>
-					<th class=" px-6 py-3">Appearance</th>
-					<th class=" px-6 py-3">Roast Recs</th>
-					<th class=" px-6 py-3">Type</th>
-					<th class=" px-6 py-3">Link</th>
-					<th class=" px-6 py-3">Purchased Qty (lbs)</th>
-					<th class=" px-6 py-3">Bean Cost</th>
-					<th class=" px-6 py-3">Tax & Ship Cost</th>
-				</tr>
-			</thead>
-			<tbody>
-				{#each data.data as bean}
-					<tr
-						class="cursor-pointer border-b border-gray-700 bg-gray-800 transition-colors hover:bg-gray-700 {selectedBean?.id ===
-						bean.id
-							? 'bg-gray-700'
-							: ''}"
-						on:click={() => selectBean(bean)}
-					>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white">{bean.id}</td>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white">{bean.name}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white">{bean.rank}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.notes}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.purchase_date}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.arrival_date}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.region}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.processing}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.drying_method}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.lot_size}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.bag_size}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.packaging}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.farm_gate}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.cultivar_detail}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.grade}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.appearance}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.roast_recs}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white">{bean.type}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white">{bean.link}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.purchased_qty_lbs}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.bean_cost}</td
-						>
-						<td class=" whitespace-nowrap text-balance px-6 py-4 text-xs text-white"
-							>{bean.tax_ship_cost}</td
-						>
+		{#if data.data.length > 0}
+			<table class:hidden={isHidden} class="table-auto rounded">
+				<thead class="bg-gray-700 text-xs uppercase text-gray-400">
+					<tr>
+						{#each Object.keys(data.data[0] || {}) as header}
+							<th class="px-6 py-3">
+								{header.replace(/_/g, ' ').replace(/\b\w/g, (c) => c.toUpperCase())}
+							</th>
+						{/each}
 					</tr>
-				{/each}
-			</tbody>
-		</table>
+				</thead>
+				<tbody>
+					{#each data.data as bean}
+						<tr
+							class="cursor-pointer border-b border-gray-700 bg-gray-800 transition-colors hover:bg-gray-700 {selectedBean?.id ===
+							bean.id
+								? 'bg-gray-700'
+								: ''}"
+							on:click={() => selectBean(bean)}
+						>
+							{#each Object.values(bean) as value}
+								<td class="whitespace-nowrap text-balance px-6 py-4 text-xs text-white">
+									{value}
+								</td>
+							{/each}
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		{:else}
+			<p class="text-white">No data available</p>
+		{/if}
 	</div>
 </div>
