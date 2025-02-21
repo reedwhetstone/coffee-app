9aa7a1e5bed16dc0b83fc45297e51d9cf4e894bd
Reed Whetstone <rwhetstone0934@gmail.com>
2025-02-21 12:20:15 -0700

    removed catchall

--- src/routes/[...catchall]/+page.ts
+++
@@ -1,9 +0,0 @@
-import { error } from '@sveltejs/kit';
-
-export function load() {
-	console.log('Catch-all route handler triggered');
-
-	const err = new Error('Page not found');
-	(err as any).code = 'NOT_FOUND';
-	throw error(404, err);
-}
