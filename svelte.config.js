2461a9386ce1992afee7ac496d65244a3104dca0
Reed Whetstone <rwhetstone0934@gmail.com>
2025-02-20 19:37:28 -0700

    vercel changes

--- svelte.config.js
+++ svelte.config.js
@@ -12,13 +12,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: vercelAdapter({
-			edge: false,
-			split: false
-		}),
-		paths: {
-			base: ''
-		}
+		adapter: vercelAdapter()
 	},
 
 	extensions: ['.svelte', '.svx']
